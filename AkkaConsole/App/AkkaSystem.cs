c41950dbced009752862121a8157b4e4baa7fa82
mrxrsd <mrxrsd@gmail.com>
2021-08-18 10:06:24 -0300

    initial structure

---
+++ AkkaConsole/App/AkkaSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+
+namespace App
+{
+    public class AkkaSystem : IHostedService
+    {
+        private readonly IServiceProvider _services;
+        private ActorSystem _actorSystem;
+
+        public AkkaSystem(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await CoordinatedShutdown.Get(_actorSystem).Run(CoordinatedShutdown.ClrExitReason.Instance);
+        }
+    }
+}
---
+++ AkkaConsole/App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((context, services) => ConfigureServicesHandler(context.Configuration, services))
+                .UseConsoleLifetime();
+        }
+
+        private static void ConfigureServicesHandler(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddHostedService<AkkaSystem>(sp => new AkkaSystem(sp));
+        }
+    }
+}
